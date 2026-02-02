f48ae6a3ddc2356361983c6eba073c4aa9a4132b
zak90847 <zs251@kent.ac.uk>
2026-02-02 13:29:54 +0000

    WORKSHOP 5

--- camel.py
+++ camel.py
@@ -1,1 +1,11 @@
-# Enter your code here
+camel_case = input("camelCase:")
+snake_case = ""
+
+for char in camel_case:
+    if char.isupper():
+        snake_case += "_" + char .lower()
+    else:
+        snake_case += char
+print("snake_case:", snake_case)
+
+
