ca5b35e161d344fb31dcd509a1619b610a98040c
zak90847 <zs251@kent.ac.uk>
2026-02-02 13:52:40 +0000

    workshop 5/

--- coffee.py
+++ coffee.py
@@ -1,1 +1,11 @@
-#Your code goes here
+def main():
+    amount_due = 75
+    accepted_coins = [50, 20, 10, 5]
+    while amount_due > 0:
+        print(f"Amount Due: {amount_due}p")
+        coin = int(input("Insert coin: "))
+        if coin in accepted_coins: amount_due -= coin
+    change = abs(amount_due)
+    print("Change Owed: {change}p")
+if __name__ ==  "__main__":
+    main()
